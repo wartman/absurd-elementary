15b7cedf8e3ba12ef1bbc6115da542d974a9e993
CSA <faxmaster@gmail.com>
2014-12-03 11:40:39 -0600

    Update

--- plugins/global.js
+++ plugins/global.js
@@ -3,6 +3,7 @@ module.exports = function (api){
   api.add({
     // Use border box.
     '*, *:before, *:after': {
+      '-wmso-box-sizing': 'border-box',
       boxSizing: 'border-box'
     },
     'img': { 
